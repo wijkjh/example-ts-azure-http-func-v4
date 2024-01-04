bedd9a8c6c2a853af79d3c595aa949bd2e10bce6
Henk Jan van Wijk <wijkjh@gmail.com>
2024-01-04 02:50:37 +0100

    feat: Add actual call to IoT Hub

--- src/functions/getDevices.ts
+++ src/functions/getDevices.ts
@@ -5,15 +5,46 @@ import {
   InvocationContext,
 } from '@azure/functions';
 
+import * as iothub from 'azure-iothub';
+import { DeviceInfo } from '../types/devices';
+
 export async function getDevices(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
+  context.log(
+    `Http function processed request for url "${request.url} ${request.method} ${request.query}"`
+  );
 
-  const name = request.query.get('name') || (await request.text()) || 'world';
+  const connectionString = process.env.IOTHUB_CONNECTION_STRING;
 
-  return { body: `Hello, ${name}!` };
+  if (connectionString) {
+    const registry = iothub.Registry.fromConnectionString(connectionString);
+    context.log('**listing devices...');
+    const listOfDevices: DeviceInfo[] = [];
+    const deviceList = await registry.list();
+    deviceList.responseBody.forEach(function (device) {
+      const key =
+        (device.authentication
+          ? device.authentication?.symmetricKey?.primaryKey
+          : '<no primary key>') || '<no key found>';
+      listOfDevices.push({
+        deviceId: device.deviceId,
+        connectionState: device.connectionState || 'unknown',
+        status: device.status || 'unknown',
+        lastActivityTime: device.lastActivityTime || 'unknown',
+        iotEdge: device.capabilities?.iotEdge || false,
+        key: request.query.has('include', 'key') ? key : undefined,
+      });
+    });
+    context.log(JSON.stringify(listOfDevices));
+    return { jsonBody: { devices: listOfDevices } };
+  } else {
+    return {
+      status: 500,
+      jsonBody: { error: 'No connection string found' },
+    };
+  }
 }
 
 app.http('getDevices', {
---
+++ src/types/devices.ts
@@ -0,0 +1,8 @@
+export type DeviceInfo = {
+  deviceId: string;
+  connectionState: string;
+  status: string;
+  lastActivityTime: string;
+  iotEdge: boolean;
+  key?: string;
+};
